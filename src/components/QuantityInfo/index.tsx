dcd4a0acb1a9bee0f029950f9753463003dd3dcb
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-26 20:42:09 -0300

    style(QuantityInfo): add responsive size

--- src/components/QuantityInfo/index.tsx
+++ src/components/QuantityInfo/index.tsx
@@ -3,10 +3,10 @@ import { QuantityInfoProps } from "./types";
 const QuantityInfo: React.FC<QuantityInfoProps> = ({ quantity, text }) => {
   return (
     <div className="flex flex-col pr-8">
-      <h2 className="text-[40px] font-bold">
+      <h2 className="text-[40px] max-sm:text-2xl font-bold">
         {quantity}+
       </h2>
-      <p className="text-black text-opacity-60 text-base">
+      <p className="text-black max-sm:text-xs text-opacity-60 text-base">
         {text}
       </p>
     </div>
