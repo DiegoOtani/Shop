191120238d3b32b067bfd8df31119e94210ad4e3
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-26 19:53:37 -0300

    feat: add QuantityInfo section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import QuantityInfo from "@/components/QuantityInfo";
+
 export default function Home() {
   return <>
-    <main>
-      
+    <main className="w-full flex flex-col items-center justify-center">
+      <section className="w-full flex justify-center space-x-8 flex-wrap">
+        <QuantityInfo quantity={"200"} text="International Brands" />
+        <div className="border-r border-gray-300"></div>
+        <QuantityInfo quantity={"2,000"} text="High-Quality Products" />
+        <div className="hidden sm:block border-r border-gray-300"></div>
+        <QuantityInfo quantity={"30,000"} text="Happy Customers" />
+      </section>
     </main>
   </>
 }
