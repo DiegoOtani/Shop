b28b97780ab6cd8e18c5eeccd8d40e9ef45e6c4c
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-26 20:22:35 -0300

    style: add responsive size

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,19 +5,19 @@ const Title: React.FC<TitleProps> = ({ text, size, color }) => {
 
   switch (size) {
     case 'large':
-      sizeClass = 'text-6xl';
+      sizeClass = 'text-6xl max-sm:text-3xl';
       break;
     case 'medium':
-      sizeClass = 'text-5xl';
+      sizeClass = 'text-5xl max-sm:text-3xl';
       break;
     case 'small':
-      sizeClass = 'text-4xl';
+      sizeClass = 'text-4xl max-sm:text-2xl';
       break;
     default:
       sizeClass = 'text-4xl';
   }
 
-  return <h1 className={`font-bold ${sizeClass} ${color === 'black' ? 'text-black' : 'text-white'}`}>
+  return <h1 className={`font-bold ${sizeClass} text- ${color === 'black' ? 'text-black' : 'text-white'}`}>
     {text}
   </h1>;
 };
