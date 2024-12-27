827331810dc7e9b672652a4048fb82b9137aa6e8
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-27 14:03:51 -0300

    style: add responsive size

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -3,7 +3,7 @@ import { PriceProps } from "./types";
 const Price: React.FC<PriceProps> = ({ price, discount }) => {
   const discountedPrice = discount ? price - (price * discount / 100) : price;
 
-  return <div className="flex gap-[10px] text-2xl">
+  return <div className="flex gap-[10px] text-2xl max-sm:text-xl">
 
     <span className="font-bold">
       ${discountedPrice}
@@ -11,7 +11,7 @@ const Price: React.FC<PriceProps> = ({ price, discount }) => {
 
     {discount && <>
         <span className="font-bold line-through text-black text-opacity-40">${price}</span>
-        <span className="text-red-500 bg-red-100 px-3 py-2 rounded-full text-xs font-medium">
+        <span className="text-red-500 bg-red-100 px-3 py-2 rounded-full text-xs font-medium max-sm:text-[10px]">
           -{discount}%
         </span>
       </>
