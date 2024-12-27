65f794921009181f5c749d6ec1d4972cb4277eda
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-26 21:00:19 -0300

    style(QuanittyInfo): removing useless padding and add min-width

--- src/components/QuantityInfo/index.tsx
+++ src/components/QuantityInfo/index.tsx
@@ -2,7 +2,7 @@ import { QuantityInfoProps } from "./types";
 
 const QuantityInfo: React.FC<QuantityInfoProps> = ({ quantity, text }) => {
   return (
-    <div className="flex flex-col pr-8">
+    <div className="flex flex-col min-w-[140px] max-sm:min-w-[120px]">
       <h2 className="text-[40px] max-sm:text-2xl font-bold">
         {quantity}+
       </h2>
