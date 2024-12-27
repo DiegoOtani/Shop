a7478ddb7d04cfe020da88e22482ef099cde20b3
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-27 15:57:23 -0300

    style: add responsive design for QuantityInfo section in HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import QuantityInfo from "@/components/QuantityInfo";
 
 export default function Home() {
   return <>
-    <main className="w-full flex flex-col items-center justify-center">
-      <section className="w-full flex justify-center space-x-8 flex-wrap">
+    <main className="w-full flex flex-col items-center justify-center p-20 max-sm:p-10">
+      <section className="w-full flex justify-center flex-wrap gap-8">
         <QuantityInfo quantity={"200"} text="International Brands" />
-        <div className="border-r border-gray-300"></div>
-        <QuantityInfo quantity={"2,000"} text="High-Quality Products" />
         <div className="hidden sm:block border-r border-gray-300"></div>
+        <QuantityInfo quantity={"2,000"} text="High-Quality Products" />
+        <div className="hidden md:block border-r border-gray-300"></div>
         <QuantityInfo quantity={"30,000"} text="Happy Customers" />
       </section>
     </main>
