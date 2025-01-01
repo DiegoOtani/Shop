467e5a859b148bfde81b425cc3b17fd010d0fc4e
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-31 22:20:35 -0300

    style(Header): add responsive layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,33 +4,63 @@ import Input from "../Input";
 import { IoIosSearch } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsPersonCircle } from "react-icons/bs";
-import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { useEffect, useState } from "react";
 import NavButton from "../NavButton";
 
 const Header = () => {
   const [search, setSearch] = useState<string>('');
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  return <header className="w-full px-24 py-10 flex items-center gap-10 border-b-black border-2">
-    <Link href={'/'} className="text-4xl font-bold">
-      SHOP.CO
-    </Link>
-    <nav>
-      <ul className="flex gap-6">
-        <NavItem href="#" text="Shop" key="Shop"/>
-        <NavItem href="#" text="On Sale" key="On Sale"/>
-        <NavItem href="#" text="New Arrivals" key="New Arrivals"/>
-        <NavItem href="#" text="Brands" key="Brands"/>
-      </ul>
-    </nav>
-    <Input 
-      icon={IoIosSearch} 
-      placeholder="Search for products..." 
-      onChange={(e) => setSearch(e.target.value)}
-      value={search}
-      onSearch={() => console.log(search)}
-    />
-    <NavButton icon={FiShoppingCart} onClick={() => console.log("Market cart button")} key="cart button"/>
-    <NavButton icon={BsPersonCircle} onClick={() => console.log("Personal account button")} key="Personal account button"/>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return <header className="w-full px-24 py-10 max-md:px-12 max-md:py-5 flex items-center justify-between gap-10 border-b-black border-2">
+
+    <div className="flex items-center gap-6">
+      {isSmallScreen && 
+        <button>
+          <IoMenu size={24}/>
+        </button>
+      }
+      
+      <Link href={'/'} className="text-4xl max-md:text-2xl font-bold">
+        SHOP.CO
+      </Link>
+    </div>
+
+    {!isSmallScreen && <>
+        <nav className="hidden sm:block">
+          <ul className="flex gap-6">
+            <NavItem href="#" text="Shop" key="Shop"/>
+            <NavItem href="#" text="On Sale" key="On Sale"/>
+            <NavItem href="#" text="New Arrivals" key="New Arrivals"/>
+            <NavItem href="#" text="Brands" key="Brands"/>
+          </ul>
+        </nav>
+        <Input 
+          icon={IoIosSearch} 
+          placeholder="Search for products..." 
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          onSearch={() => console.log(search)}
+        />
+      </>
+    }
+
+    <div className="flex gap-6">
+      {isSmallScreen && <NavButton icon={IoIosSearch} onClick={() => setIsSmallScreen(true)} key="search button"/>}
+      <NavButton icon={FiShoppingCart} onClick={() => console.log("Market cart button")} key="cart button"/>
+      <NavButton icon={BsPersonCircle} onClick={() => console.log("Personal account button")} key="Personal account button"/>
+    </div>
   </header>
 }
 
