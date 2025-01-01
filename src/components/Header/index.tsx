3aa43cc5c0fce90840718aaaceb4b224b2640fde
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-12-31 23:26:57 -0300

    feat(Header): add menu modal opened by Hamburguer Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,14 @@ import Input from "../Input";
 import { IoIosSearch } from "react-icons/io";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsPersonCircle } from "react-icons/bs";
-import { IoMenu } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 import { useEffect, useState } from "react";
 import NavButton from "./components/NavButton";
 
 const Header = () => {
   const [search, setSearch] = useState<string>('');
   const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -23,11 +24,19 @@ const Header = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const handleOpenMenu = () => {
+    setIsMenuOpen(true);
+  }
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return <header className="w-full px-12 py-8 max-md:px-5 max-md:py-5 flex items-center justify-between gap-10 border-b-black border-2">
 
     <div className="flex items-center gap-6 max-md:gap-3">
       {isSmallScreen && 
-        <button>
+        <button onClick={handleOpenMenu}>
           <IoMenu size={24}/>
         </button>
       }
@@ -61,6 +70,33 @@ const Header = () => {
       <NavButton icon={FiShoppingCart} onClick={() => console.log("Market cart button")} key="cart button"/>
       <NavButton icon={BsPersonCircle} onClick={() => console.log("Personal account button")} key="Personal account button"/>
     </div>
+
+    {isMenuOpen && (
+        <div
+          className="fixed inset-0 z-50 flex"
+          onClick={handleCloseMenu}
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-50" />
+
+          <div
+            className="relative w-64 h-full bg-white shadow-lg flex flex-col p-6"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button className="self-end mb-4" onClick={handleCloseMenu}>
+              <IoClose size={24} />
+            </button>
+            <Link href={'/'} className="text-4xl max-md:text-2xl font-bold mb-6">
+              SHOP.CO
+            </Link>
+            <nav className="flex flex-col gap-4 ">
+              <NavItem href="#" text="Shop" />
+              <NavItem href="#" text="On Sale" />
+              <NavItem href="#" text="New Arrivals" />
+              <NavItem href="#" text="Brands" />
+            </nav>
+          </div>
+        </div>
+      )}
   </header>
 }
 
