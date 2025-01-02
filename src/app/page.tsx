253683a290d8b9f6e84b80afe8eeba7cf8436966
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-01-02 09:30:57 -0300

    style: adjust main padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import QuantityInfo from "@/components/QuantityInfo";
 
 export default function Home() {
   return <>
-    <main className="w-full flex flex-col items-center justify-center p-20 max-sm:p-10">
+    <main className="w-full flex flex-col items-center justify-center p-12 max-md:p-5">
       <section className="w-full flex justify-center flex-wrap gap-8">
         <QuantityInfo quantity={"200"} text="International Brands" />
         <div className="hidden sm:block border-r border-gray-300"></div>
